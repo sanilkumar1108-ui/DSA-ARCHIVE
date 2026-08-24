6ebbaa07b60e073bae73752997a903390718eed5
Sanil kumar <sanilkumar1108@gmail.com>
2026-08-24 18:14:22 +0530

    Time: 12 ms (95.09%), Space: 48.5 MB (15.17%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        ArrayDeque<Adjacent> st = new ArrayDeque<>(s.length());
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peekLast().ch == c) {
+                st.peekLast().freq++; // Increase the frequency
+            } else {
+                st.addLast(new Adjacent(c, 1));
+            }
+            if (st.peekLast().freq == k) // If reach enough k duplicate letters -> then remove
+                st.removeLast();
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Adjacent a : st) {
+            sb.append(String.valueOf(a.ch).repeat(a.freq));
+        }
+        return sb.toString();
+    }
+    class Adjacent {
+        char ch;
+        int freq;
+        public Adjacent(char ch, int freq) {
+            this.ch = ch;
+            this.freq = freq;
+        }
+    }
+    }
